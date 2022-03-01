ee53380c99043f6553aa3f551f54658e77cafa1e
VuqarTagiyev <vugartaghiyev@mail.ru>
2022-03-01 12:56:19 +0400

    build problem solved

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -3,7 +3,7 @@ import Footer from "./Footer";
 import Header from "./Header";
 
 // styels
-import styles from "./Layout.module.css";
+import styles from "./layout.module.css";
 
 const Layout = ({ children }) => {
   return (
